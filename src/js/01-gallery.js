4cfec513d2e67dcce726ffb5d070f4551869c241
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-21 18:00:16 +0300

    update task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const gallaryMarkup = galleryCreateMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+function galleryCreateMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+        </a></li>`;
+    })
+    .join('');
+}
+
+galleryRef.addEventListener('click', onOpenPictureClick);
+
+function onOpenPictureClick(e) {
+  e.preventDefault();
+  e.target.alt;
+  console.log(e.target.alt);
+}
+
+let lightboxPicture = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+lightboxPicture.on('show.simpleLightbox');
