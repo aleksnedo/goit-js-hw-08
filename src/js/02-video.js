e0df9bb1794351fb9c97396260e8d96887371d2e
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-21 21:45:00 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,3 +3,7 @@ import throttle from 'lodash.throttle';
 
 const iframeRef = document.querySelector('#vimeo-player');
 const player = new VimeoPlayer(iframeRef);
+
+player.on('eventName', function () {
+  console.log(`Played the video`);
+});
