98c6d9984f79c1d7289540d892c07e4556d3106b
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-21 19:25:15 +0300

    add lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframeRef = document.querySelector('#vimeo-player');
-
 const player = new VimeoPlayer(iframeRef);
